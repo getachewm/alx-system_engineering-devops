366406529deec531cacab5bd6c45db335c2b705e
root <getachew.mekonen12@gmail.com>
2023-12-31 21:42:57 -0800

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 # using puppet to make changes in our config file
 
-file  {'etc/ssh/ssh_config':
+file  { 'etc/ssh/ssh_config':
 	ensure => present,
 
-contents =>"
+content =>"
 
 	#SSH client configuration
 	host*
-	IdentityFile -/.ssh/school
+	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
 	",
 }
